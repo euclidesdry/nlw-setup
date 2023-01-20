8dc39f09c9d44b003f987538cf2817508bdf67aa
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-20 13:26:58 +0100

    feat: ✨ adding database into the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,28 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Hello world!";
+app.register(cors);
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "beber",
+      },
+    },
+  });
+
+  return habits;
 });
 
-app.listen({
-  port: 3333,
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running on\x1B[34m http://localhost:3333");
+  });
