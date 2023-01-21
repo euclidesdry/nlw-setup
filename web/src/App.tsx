26616ccb528545e904c7a83dcbe483f850441172
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-21 20:42:54 +0100

    feat(web): ✨ adding daily habits table component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
 // import { Habit } from "./components/Habit";
 
 import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-5xl px-6 flex flex-col gap-1">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-8">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   );
