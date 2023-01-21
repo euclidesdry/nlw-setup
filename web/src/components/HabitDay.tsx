33a4ff21301b3842130f5656b4ae4c834f064e13
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-21 21:12:10 +0100

    feat(web): ✨ making the fill of rest of days

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,13 @@
 interface HabitDayProps {
-  completed?: number;
+  disabled?: boolean;
 }
 
-export function HabitDay({ completed }: HabitDayProps) {
+export function HabitDay({ disabled }: HabitDayProps) {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <div
+      className={`w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg ${
+        disabled ? "opacity-40 cursor-not-allowed" : ""
+      }`}
+    ></div>
   );
 }
